8fd9dec6db6e53c6176f06eb751d1c213633165c
David McMackins <mcmackinsd@gmail.com>
2014-01-20 13:17:16 -0600

    Created framework and started with files

--- src/base/plugin/Taunter.java
+++ src/base/plugin/Taunter.java
@@ -6,6 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -14,10 +15,51 @@ import java.util.ArrayList;
 public class Taunter extends JavaPlugin
 {
     private ArrayList<String> taunts;
+    private File folder, login, death;
 
     Taunter()
     {
+        folder = new File("plugins" + File.separator + "Taunter");
+        login = new File(folder, "loginmessages.yml");
+        death = new File(folder, "deathmessages.yml");
         // todo: read YML config for taunt list
+    }
+
+    public void onEnable()
+    {
+        generateDefaults();
+    }
+
+    private void generateDefaults()
+    {
+
+        // make working directory
+        if (!folder.exists())
+        {
+            try
+            {
+                folder.mkdir();
+            }
+            catch (Exception e)
+            {
+                getLogger().severe("Could not create working directory for Taunter!");
+                getPluginLoader().disablePlugin(this);
+            }
+        }
+
+        // generate config
+        if (!getConfig().get("loginmessages").equals(true) && !getConfig().get("loginmessages").equals(false))
+        {
+            getConfig().set("loginmessages", true);
+        }
+        if (!getConfig().get("deathmessages").equals(true) && !getConfig().get("deathmessages").equals(false))
+        {
+            getConfig().set("deathmessages", true);
+        }
+        saveConfig();
+
+        // generate login messages file
+        // generate death messages file
     }
 
     private void sendLoginTaunt(Player x)
